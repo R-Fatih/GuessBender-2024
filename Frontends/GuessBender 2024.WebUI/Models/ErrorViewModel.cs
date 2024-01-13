f457f0947a40dc09c6dd66b2d7ecb33483b61815
RFatih <39864827+R-Fatih@users.noreply.github.com>
2024-01-14 01:48:35 +0300

    Proje dosyası ekle.

---
+++ Frontends/GuessBender 2024.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuessBender_2024.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
