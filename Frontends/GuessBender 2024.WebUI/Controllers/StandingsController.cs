43f74c93d9b4ab82dc8244c5c1d7ce27a787f1a3
RFatih <39864827+R-Fatih@users.noreply.github.com>
2024-01-21 20:21:02 +0300

    up

--- Frontends/GuessBender 2024.WebUI/Controllers/StandingsController.cs
+++ Frontends/GuessBender 2024.WebUI/Controllers/StandingsController.cs
@@ -10,7 +10,7 @@ namespace GuessBender_2024.WebUI.Controllers
     [Authorize(Roles = "Admin, Comp")]
 
     public class StandingsController : BaseController
-	{
+    {
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly ILogger<StandingsController> _logger;
         public StandingsController(IHttpClientFactory httpClientFactory, ILogger<StandingsController> logger)
@@ -36,7 +36,7 @@ namespace GuessBender_2024.WebUI.Controllers
                     var values = JsonConvert.DeserializeObject<List<ResultStandingDto>>(jsonData);
                     _logger.LogInformation($"Standings visited by {User.Identity?.Name}");
 
-            
+
 
                     return View(values);
                 }
@@ -46,5 +46,42 @@ namespace GuessBender_2024.WebUI.Controllers
             return View();
         }
 
-	}
+        [HttpPost]
+        public Task<IActionResult> Index(string username)
+        {
+            TempData["username"] = username;
+            var a = "";
+
+            return Task.FromResult<IActionResult>(RedirectToAction("MatchDetails", "Standings"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MatchDetails()
+        {
+            var username = TempData["username"];
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken").Value;
+            if (token != null)
+            {
+
+
+                var client = _httpClientFactory.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var responseMessage = await client.GetAsync($"https://localhost:7185/api/Standings/GetStandingMatchesByUsername?username="+username);
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<List<ResultStandingMatchesWithDetailsByUsernameDto>>(jsonData);
+                    _logger.LogInformation($"Match details visited by {User.Identity?.Name}");
+
+
+
+                    return View(values);
+                }
+                _logger.LogError($"There is a problem on Match details request");
+
+            }
+            return View();
+        }
+    }
 }
